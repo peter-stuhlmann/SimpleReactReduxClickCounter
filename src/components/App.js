04246b8665f8c6aa77e0a8d0a03065bc056d1810
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-27 17:29:31 +0200

    Init React project
    Install:
    - npm i create-react-app
    - npm i node-sass (to use scss)
    
    Customize React App:
    - Create two folders in src: components/ and assets/ respectively assets/scss/
    - Move App.js in components/ and add a class App with an empty render function; update import path in index.js
    - Create empty main.scss in assets/scss/ and import it in App.js
    
    Remove unneeded files:
    - Remove logo.svg from src/ and the logo import in index.js
    - Remove favicon.ico and manifest.json from public/
    - Remove serviceWorker

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../assets/scss/main.scss"
+
+export default class App extends Component {
+
+  render() {
+    return ( 
+      <React.Fragment>
+        Hello World!
+      </React.Fragment>
+    )
+  }
+
+}
