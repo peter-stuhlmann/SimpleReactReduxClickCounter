c86fb2881816c4523304f51ea243ab6b91c35d81
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-27 22:53:26 +0200

    Outsource code in a separate file
    Create a function incrementCounter in actions/index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { createStore } from "redux";
+import { incrementCounter } from "./actions/index";
 import Counter from "./reducers/index";
 
 let store = createStore(Counter);
 console.log(store.getState());
 
-store.dispatch({ type: "INCREMENT" }); 
+store.dispatch(incrementCounter()); 
 console.log(store.getState());
 
 let render = () => {
     ReactDOM.render(
         <App 
             value={store.getState()}
-            onIncrement={() => {store.dispatch({ type: "INCREMENT" });}}
+            onIncrement={() => {store.dispatch(incrementCounter());}}
         />, 
         document.querySelector("#root")
     );
