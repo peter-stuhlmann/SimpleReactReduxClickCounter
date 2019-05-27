8edf99687a53997153302b321283872ab740a065
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-27 23:16:27 +0200

    Connect App component
    { connect } from 'react-redux'

--- src/index.js
+++ src/index.js
@@ -2,24 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { createStore } from "redux";
+import { connect } from "react-redux";
 import { incrementCounter } from "./actions/index";
 import Counter from "./reducers/index";
 
 let store = createStore(Counter);
 console.log(store.getState());
 
-store.dispatch(incrementCounter()); 
-console.log(store.getState());
+let mapStateToProps = function(state) {
+    return {
+        value: state
+    }
+}
+
+let mapDispatchToProps = {
+    onIncrement: incrementCounter
+}
+
+let App2 = connect(mapStateToProps, mapDispatchToProps)(App);
 
 let render = () => {
-    ReactDOM.render(
-        <App 
-            value={store.getState()}
-            onIncrement={() => {store.dispatch(incrementCounter());}}
-        />, 
-        document.querySelector("#root")
-    );
+    ReactDOM.render(<App2 store={store} />, document.querySelector("#root"));
 }
 
 render();
-store.subscribe(render);
