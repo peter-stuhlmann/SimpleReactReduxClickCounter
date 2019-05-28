b0da4243032839f364eb120462250e3e179b54ad
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-05-28 14:30:34 +0200

    Add configuration for Redux DevTools extension

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import Counter from "./reducers/index";
 
-let store = createStore(Counter);
+let store = createStore(Counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 console.log(store.getState());
 
 ReactDOM.render(
